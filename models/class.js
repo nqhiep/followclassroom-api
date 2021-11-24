77df08d08e1f94ce3e3fae7b300bdbbd886b14cc
Hieu Nguyen <minhhieunguyen23121999@gmail.com>
2021-11-24 09:00:51 +0700

    update: detail edit

--- models/class.js
+++ models/class.js
@@ -17,6 +17,7 @@ module.exports = (sequelize, DataTypes) => {
         cover: DataTypes.STRING,
         student_link: DataTypes.STRING,
         teacher_link: DataTypes.STRING,
+        
     }, {
         sequelize,
         modelName: 'Classes',
